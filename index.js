9912c52ed461df00d0d5a5bbddbd6cbb986934a5
aseelflihan <aseelflihan@gmail.com>
2023-01-30 18:24:26 +0300

    complated assginment

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const sum = function (preValue, currentValue) {
+    return preValue + currentValue;
+  };
+  
+  const totalBatteries = batteryBatches.reduce(sum, 0);
+  
+  console.log(totalBatteries);
